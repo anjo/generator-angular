f9f7f79ed3db0ff4ac431d7a6b79a268392499e1
anjo <anjo@krank.net>
2013-02-11 15:52:32 +0100

    component added (controller+ui)

---
+++ component/index.js
@@ -0,0 +1,24 @@
+var path = require('path'),
+    util = require('util'),
+    grunt = require('grunt'),
+    yeoman = require('../../../../'),
+    ScriptBase = require('../script-base.js');
+angularUtils = require('../util.js');
+
+module.exports = Generator;
+
+function Generator() {
+    ScriptBase.apply(this, arguments);
+    this.sourceRoot(path.join(__dirname, '../templates'));
+
+    this.appname = path.basename(process.cwd());
+
+    this.hookFor('angular:controller', {
+        args: [this.name]
+    });
+    this.hookFor('angular:view', {
+        args: [this.name]
+    });
+}
+
+util.inherits(Generator, ScriptBase);
